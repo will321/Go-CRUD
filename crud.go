58bf05b1c3e4ed873c1ebddfd0911657fef10ecd
will321 <55818403+will321@users.noreply.github.com>
2025-02-02 13:06:02 -0500

    update

--- crud.go
+++ crud.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("Hello, World!")
-
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
 }
